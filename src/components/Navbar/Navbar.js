fdb95187db86b99663179f8841591c2fd7d88a18
Eric Keyes <keyes.eric@outlook.comgit config --global user.email keyes.eric@outlook.com>
2018-12-17 11:14:22 -0500

    completed navbar, app.js updated

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Navbar = props => (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <h1 className="navbar-brand">The Clicky Game!</h1>
+    </nav>
+);
+
+export default Navbar;
