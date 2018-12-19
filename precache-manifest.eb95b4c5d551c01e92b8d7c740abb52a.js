e70c70fc9d235ec708710054ae6dd52043317795
Eric Keyes <keyes.eric@outlook.comgit config --global user.email keyes.eric@outlook.com>
2018-12-19 18:08:03 -0500

    Updates

---
+++ precache-manifest.eb95b4c5d551c01e92b8d7c740abb52a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "43588a4385ba6e1c69cbb956d1d44219",
+    "url": "/clicky-game/static/media/leia.43588a43.png"
+  },
+  {
+    "revision": "42fc45ea196e5a0ebf67",
+    "url": "/clicky-game/static/css/main.b6d5753f.chunk.css"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/clicky-game/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "dd2dc3e0d24b25f88783e07e24684d7e",
+    "url": "/clicky-game/static/media/ackbar.dd2dc3e0.png"
+  },
+  {
+    "revision": "d1536a0706cd6e5fc647b15555a13496",
+    "url": "/clicky-game/static/media/boba.d1536a07.png"
+  },
+  {
+    "revision": "533a7b9d196b658de1cfe222efd3ab91",
+    "url": "/clicky-game/static/media/chewbacca.533a7b9d.png"
+  },
+  {
+    "revision": "b53451058908372dee7c2047a41054e3",
+    "url": "/clicky-game/static/media/hansolo.b5345105.png"
+  },
+  {
+    "revision": "42fc45ea196e5a0ebf67",
+    "url": "/clicky-game/static/js/main.42fc45ea.chunk.js"
+  },
+  {
+    "revision": "884455e98465de23b9f0b7fc3e22064b",
+    "url": "/clicky-game/static/media/luke.884455e9.png"
+  },
+  {
+    "revision": "a3651dd2703acae370bd059ed131fd84",
+    "url": "/clicky-game/static/media/obiwan.a3651dd2.png"
+  },
+  {
+    "revision": "220ef5c6ed369a2dadaf958ff9f30d8a",
+    "url": "/clicky-game/static/media/palpatine.220ef5c6.png"
+  },
+  {
+    "revision": "b0de18bafcab9c2ea3562eb730e38ea5",
+    "url": "/clicky-game/static/media/r2d2.b0de18ba.png"
+  },
+  {
+    "revision": "6df8435848356c30ec57aa64fb87e96c",
+    "url": "/clicky-game/static/media/tarkin.6df84358.png"
+  },
+  {
+    "revision": "1bfddd6597aa07851a62d1f466f5c0d5",
+    "url": "/clicky-game/static/media/vader.1bfddd65.png"
+  },
+  {
+    "revision": "c40e0e19902531d723a7a2654a1747f7",
+    "url": "/clicky-game/static/media/yoda.c40e0e19.png"
+  },
+  {
+    "revision": "c7285096e601ee2891bdc18df8c2c97d",
+    "url": "/clicky-game/index.html"
+  }
+];
